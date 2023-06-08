afeb8eda5659f1770ce7286aedaeb0866fdaf4a6
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-08 14:49:02 -0500

    Add reactionBody and createdAt to ReactionSchema

--- models/reactions.js
+++ models/reactions.js
@@ -12,6 +12,21 @@ const ReactionSchema = new Schema({
         required: true,
         trim: true,
     },
+    reactionBody: {
+        type: String,
+        required: true,
+        trim: true,
+        match: /^[\w-]{1,280}$/,
+        // minLength: 1,
+        // maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: function(date){
+            return date.toLocaleDateString()
+        }
+    },
 })
 
 
