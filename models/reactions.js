80cea0ed185e1f4ea46d590fd42662e2daa045d0
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-08 15:07:33 -0500

    Attempting to fix merge conflict

--- models/reactions.js
+++ models/reactions.js
@@ -1,4 +1,39 @@
 const { Schema, Types } = require('mongoose');
 
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> {
+            return new Types.ObjectId()
+        }
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        trim: true,
+        match: /^[\w-]{1,280}$/,
+        // minLength: 1,
+        // maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: function(date){
+            return date.toLocaleDateString()
+        }
+    },
+},
+{
+    toJSON: {
+        getters: true,
+    }
+});
+
+module.exports = ReactionSchema;
 
 
