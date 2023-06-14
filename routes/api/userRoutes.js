5966471a57bab8cdf76e0744cbc45cbd5fe222e3
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-13 19:08:53 -0500

    Add logic for adding/deleting friends

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,13 +5,17 @@ const {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/userController');
 
 router.route('/').get(getAllUsers).post(createUser);
 
 
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 // router.post('/', createUser);
 
