2a4020d25256e412fdc073e47d9c349c6601f182
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-13 19:51:47 -0500

    Finished the code for the routes in the thoughtRoutes file

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,7 +5,9 @@ const {
     getThoughtById,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+    deleteReaction
 }   = require('../../controllers/thoughtController');
 
 
@@ -15,9 +17,11 @@ router.route('/').get(getAllThoughts).post(createThought);
 // router.post('/', createThought);
 
 router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
-
 // router.get('/:id', getThoughtById);
 // router.put('/:id', updateThought);
 // router.delete('/:id', deleteThought);
+
+router.route('/:id/reactions').post(addReaction).delete(deleteReaction);
+
 
 module.exports = router;
